542c6a2031b52792e5bf141fdeaee3b42ef4c94d
Mityasaurus <dmitriybovdun5@gmail.com>
2024-02-22 20:45:53 +0200

    Homework 18 02 24 completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import s from "../style/footer.module.css";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className={s.footer}>
+      <img
+        src="https://www.computerhope.com/jargon/g/guest-user.png"
+        alt="Profile"
+      />
+      <article>
+        <p className={s.text_name}>Evano</p>
+        <p className={s.text_desc}>Project manager</p>
+      </article>
+    </footer>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import s from "../style/header.module.css";
+import logoImg from "../assets/img/logo.png";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className={s.header}>
+      <img src={logoImg} alt="Logo" />
+      <h1>Dashboard</h1>
+    </header>
+  );
 }
